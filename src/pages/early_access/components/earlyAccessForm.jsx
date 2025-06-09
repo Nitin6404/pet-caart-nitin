2a5722e1eef79ae85f5a2ee5a4daf10d716cfd7f
Nitin Chaudhary <nc1575612@gmail.com>
2025-06-09 23:16:23 +0530

    refactor: Improve layout and styling of Early Access form component

--- src/pages/early_access/components/earlyAccessForm.jsx
+++ src/pages/early_access/components/earlyAccessForm.jsx
@@ -7,21 +7,16 @@ const EarlyAccessForm = () => {
   const [showForm, setShowForm] = useState(false);
 
   return (
-    <div className=" w-screen h-screen flex items-center justify-center"
-      
-    >
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
-        <img src="/exp2.jpg"  alt="" style={{
-        // backgroundImage: "url('/exp2.jpg')",
-        backgroundRepeat: "no-repeat",
-        // objectPosition: "40% 50%",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        opacity: 0.1,
-        position: "absolute",
-        overflow: "hidden",
-      }}/>
+    <div className=" w-screen h-screen flex items-center justify-center">
+      <div className="absolute inset-0">
+        <img
+          src="/exp2.jpg"
+          alt=""
+          className="w-full h-full object-cover opacity-10"
+          loading="lazy"
+        />
       </div>
+
       {showForm && <EarlyAccessDialog onClose={() => setShowForm(false)} />}
 
       <div className="w-full max-w-md h-full flex flex-col justify-between items-center px-4 py-6">
@@ -35,13 +30,24 @@ const EarlyAccessForm = () => {
           <div className="mb-4 text-4xl">👍</div>
 
           <div className="space-y-2 mb-6">
-            <p className="uppercase font-bold text-gray-600 tracking-wide">We&apos;re still</p>
-            <h1 className="text-3xl md:text-4xl font-bold text-blue-600">Cooking Our Website.</h1>
-            <p className="text-gray-600">We are going to launch our website very soon.</p>
+            <p className="uppercase font-bold text-gray-600 tracking-wide">
+              We&apos;re still
+            </p>
+            <h1 className="text-3xl md:text-4xl font-bold text-blue-600">
+              Cooking Our Website.
+            </h1>
+            <p className="text-gray-600">
+              We are going to launch our website very soon.
+            </p>
             <p className="text-gray-600">Stay tuned.</p>
           </div>
 
-          <form onSubmit={(e) => { e.preventDefault(); setShowForm(true); }}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setShowForm(true);
+            }}
+          >
             <button
               type="submit"
               className="bg-gray-900 text-white py-2 pr-6 pl-3 rounded-full flex items-center gap-5 hover:bg-gray-800 transition-all duration-300 shadow-md hover:shadow-lg scale-100 hover:scale-105 transform"
